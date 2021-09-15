fba889d835a96053b11a8a59cc8b5efd1c76fc1c
Usman Noor <usmannoor519@gmail.com>
2021-09-15 13:41:23 +0500

    complete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import produce from "immer";
 
 function App() {
+  const [info, setInfo] = useState({
+    name: "",
+    age: "",
+  });
+  const [data, setData] = useState([]);
+
+  const handleInput = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setInfo(
+      produce(info, (draft) => {
+        draft[name] = value;
+      })
+    );
+    //setInfo({ ...info, [name]: value });
+  };
+
+  const onSubmit = () => {
+    setData(
+      produce(data, (draft) => {
+        draft.push(info);
+      })
+    );
+    //setData([...data, info]);
+
+    setInfo(
+      produce(info, (draft) => {
+        draft.name = "";
+        draft.age = "";
+      })
+    );
+    //setInfo({ name: "", age: "" });
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input
+        value={info.name}
+        type="text"
+        placeholder="Enter Name..."
+        name="name"
+        onChange={handleInput}
+      ></input>
+      <br />
+      <input
+        value={info.age}
+        type="number"
+        placeholder="Enter Age..."
+        name="age"
+        onChange={handleInput}
+      ></input>
+
+      <br />
+      <button onClick={onSubmit}>Submit</button>
+      <div>
+        {data.map((obj) => (
+          <div>
+            <ul>
+              <li>{obj.name}</li>
+              <li>{obj.age}</li>
+            </ul>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
